b87b1e44e4e090ff51e5989b2cbb2d28e76db49e
thomasbibi <thomasbibin38@gmail.com>
2023-05-20 19:58:23 +0530

    Added middleware and modals

---
+++ Backend/Middlewares/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken')
+
+const jwtAuth = (req,res,next)=>{
+    try{
+        let token = req.headers.autherization
+
+        if(!token){
+            return res.status(403).json({
+                message : 'Access forbidden'
+            })
+        }
+
+        if(token.startsWith("Bearer ")){
+            token = token.split(" ")[1]
+        }
+
+        const verifyUser = jwt.verify(token,process.env.JWT_SECRET)
+
+        if(verifyUser){
+            res.user = verifyUser
+            next()
+        }
+    }
+
+    catch(error){
+        res.status(500).json({
+            error : error.message 
+        })
+    }
+}
+
+module.exports = jwtAuth;
